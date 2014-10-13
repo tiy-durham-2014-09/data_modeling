90ef9d80289cbd6a0e0a035be4c1dd7f683be3e1
David Perry <curiousepic@gmail.com>
2014-10-13 18:07:18 -0400

    Fixed Dependent Syntax

--- app/models/body.rb
+++ app/models/body.rb
@@ -3,5 +3,5 @@ class Body < ActiveRecord::Base
   belongs_to :ship
   belongs_to :parent_body, class_name: "Body"
   has_many :sub_bodies, class_name: "Body", foreign_key: "parent_body_id"
-  has_many :space_barnacles, as: :barnable, dependent: destroy
+  has_many :space_barnacles, as: :barnable, dependent: :destroy
 end
--- app/models/crew_member.rb
+++ app/models/crew_member.rb
@@ -2,5 +2,5 @@ class CrewMember < ActiveRecord::Base
   belongs_to :ship
   belongs_to :captain, class_name: "CrewMember"
   has_many :subordinates, class_name: "CrewMember", foreign_key: :captain_id
-  has_many :space_barnacles, as: :barnable, dependent: destroy
+  has_many :space_barnacles, as: :barnable, dependent: :destroy
 end
