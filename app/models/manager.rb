7520eff8116a28f99d9518cf20ec2234b0fbf0b2
Noah Holmes <noah.r.holmes@gmail.com>
2014-10-13 23:05:14 -0400

    added seeds

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,8 +1,9 @@
 class Manager < ActiveRecord::Base
+	belongs_to :ceo, class_name: "Manager"
 	belongs_to :company
 	has_many :employees
 
-	validates :name, presence: :true
-	validates :badge_number, presence: :true
-	validates :badge_number, numericality: :true
+	validates :name, presence: true
+	validates :badge_number, presence: true#, ploymorphic: true #brings up error in consolemanager
+	validates :badge_number, numericality: true
 end
