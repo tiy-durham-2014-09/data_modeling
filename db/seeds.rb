caf4a3a927bf23174edb5c1e78276ecaffb0d2c0
David Perry <curiousepic@gmail.com>
2014-10-13 23:12:05 -0400

    Seed data for Regions, Stars, Planets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Region.delete_all
+Body.delete_all
+Ship.delete_all
+CrewMember.delete_all
+SpaceBarnacle.delete_all
+
+@regions = []
+
+region_names = ['Region X', 'Nebular Deeps', 'Lumpy Zone']
+region_names.each do |r|
+  region = Region.create!(name: r)
+  @regions << region
+end
+
+@stars = []
+
+@regions.each do |r|
+  rand(2..5).times do
+    star = r.bodies.create!(name: ['HIP - ', 'Gliese ', 'DC-'].sample + rand(1000..9999).to_s, size: ['Supergiant', 'Middling Sun', 'Crescent Star', 'Tiny Dwarf Star'].sample )
+    @stars << star
+  end
+end
+
+@planets = []
+
+@stars.each do |s|
+  rand(0..3).times do
+    planet = s.sub_bodies.create!(name: s.name + "-" + ['A', 'B', 'C', 'D', 'E', 'F'].sample, size: ['Cloud Giant','Chunk of Metal','Bushy Mass','Crystal Sphere','Boring-class Planet','Cyberstation'].sample)
+    @planets << planet
+  end
+end
+
