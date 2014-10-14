edbdac5366fb78069f3bce4f92157543070142ee
GeoffPurdy <geoff_purdy@yahoo.com>
2014-10-14 08:36:31 -0400

    added isbn format registration to Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ActiveRecord::Base
   belongs_to :publisher
   validates :title, presence: true
+  validates_format_of :isbn, :with => /^(97(8|9))?\d{9}(\d|X)$/, :on => :create
   has_many :authors, through: :book_authors
   has_many :formats, through: :book_formats
   has_one  :prev_edition, class_name: "Book",
