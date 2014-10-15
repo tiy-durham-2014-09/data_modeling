6c9370a09981b30301d00604d288b4b942e1645e
GeoffPurdy <geoff_purdy@yahoo.com>
2014-10-14 20:25:49 -0400

    changed book model to allow null isbn;  added db seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require "faker"
+
+# BookAuthor.delete_all
+# BookFormat.delete_all
+# Book.delete_all
+# Format.delete_all
+# Author.delete_all
+# Publisher.delete_all
+
+formats = ['print', 'audio', 'epub']
+
+10.times do
+
+  n = Faker::Name.name
+
+  a = Author.create!(name: n, deceased_date: Faker::Date.between(150.years.ago, 20.years.ago))
+
+  p = Publisher.create!(name: Faker::Company.name, address: Faker::Address.to_s)
+
+  f = Format.create!(name: formats.sample)
+
+  b = Book.create!(title: Faker::Company.catch_phrase, publication_date: Faker::Date.between(100.years.ago, 0.years.ago))
+
+  ba = BookAuthor.create!(book: b, author: a)
+
+  bf = BookFormat.create!(book: b, format: f)
+
+end
